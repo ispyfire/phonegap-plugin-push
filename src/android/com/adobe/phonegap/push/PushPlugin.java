57a59ccae23a0c40a1894d9f2a6d9256d9df6015
Matt Sousley <matt@cmproductions.net>
2018-06-14 07:49:01 -0700

    small tweak for sound

--- src/android/com/adobe/phonegap/push/PushPlugin.java
+++ src/android/com/adobe/phonegap/push/PushPlugin.java
@@ -88,10 +88,12 @@ public class PushPlugin extends CordovaPlugin implements PushConstants {
   private void createChannel(JSONObject channel) throws JSONException {
     // only call on Android O and above
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      Log.i(LOG_TAG, "createChannel");
       final NotificationManager notificationManager = (NotificationManager) cordova.getActivity()
           .getSystemService(Context.NOTIFICATION_SERVICE);
 
       String packageName = getApplicationContext().getPackageName();
+      Log.i(LOG_TAG, "createChannel packageName=" + packageName);
       NotificationChannel mChannel = new NotificationChannel(channel.getString(CHANNEL_ID),
           channel.optString(CHANNEL_DESCRIPTION, ""),
           channel.optInt(CHANNEL_IMPORTANCE, NotificationManager.IMPORTANCE_DEFAULT));
@@ -108,6 +110,7 @@ public class PushPlugin extends CordovaPlugin implements PushConstants {
       mChannel.setShowBadge(badge);
 
       String sound = channel.optString(SOUND, "default");
+      Log.i(LOG_TAG, "createChannel sound=" + sound);
       AudioAttributes audioAttributes = new AudioAttributes.Builder()
           .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
           .setUsage(AudioAttributes.USAGE_NOTIFICATION_RINGTONE).build();
@@ -115,6 +118,7 @@ public class PushPlugin extends CordovaPlugin implements PushConstants {
         mChannel.setSound(android.provider.Settings.System.DEFAULT_RINGTONE_URI, audioAttributes);
       } else if (sound != null && !sound.contentEquals(SOUND_DEFAULT)) {
         Uri soundUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + packageName + "/raw/" + sound);
+        Log.i(LOG_TAG, "createChannel soundUri=" + ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + packageName + "/raw/" + sound);
         mChannel.setSound(soundUri, audioAttributes);
       } else {
         mChannel.setSound(android.provider.Settings.System.DEFAULT_NOTIFICATION_URI, audioAttributes);
@@ -123,6 +127,7 @@ public class PushPlugin extends CordovaPlugin implements PushConstants {
       // If vibration settings is an array set vibration pattern, else set enable vibration.
       JSONArray pattern = channel.optJSONArray(CHANNEL_VIBRATION);
       if (pattern != null) {
+        Log.i(LOG_TAG, "createChannel vibrate=pattern");
         int patternLength = pattern.length();
         long[] patternArray = new long[patternLength];
         for (int i = 0; i < patternLength; i++) {
@@ -132,8 +137,10 @@ public class PushPlugin extends CordovaPlugin implements PushConstants {
       } else {
         boolean vibrate = channel.optBoolean(CHANNEL_VIBRATION, true);
         mChannel.enableVibration(vibrate);
+        Log.i(LOG_TAG, "createChannel vibrate=" + vibrate);
       }
 
+      Log.i(LOG_TAG, "createChannel finished");
       notificationManager.createNotificationChannel(mChannel);
     }
   }
@@ -146,7 +153,7 @@ public class PushPlugin extends CordovaPlugin implements PushConstants {
       final NotificationManager notificationManager = (NotificationManager) cordova.getActivity()
           .getSystemService(Context.NOTIFICATION_SERVICE);
       List<NotificationChannel> channels = notificationManager.getNotificationChannels();
-      
+
       for (int i=0; i<channels.size(); i++ ) {
         id = channels.get(i).getId();
         if (id.equals(DEFAULT_CHANNEL_ID)) {
